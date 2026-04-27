f3388d3130c93fcac99120243e80993d1bdba343
Drahiri <kodzidig@o2.pl>
2026-04-27 14:13:13 +0200

    Add variable 'age'

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -14,6 +14,7 @@ const thisYear = 1965;
 const birthYear = 1947;
 
 const fullName = firstName.concat(" ", lastName);
+const age = thisYear - birthYear;
 
 const greeting = `Hello! My name is ${firstName} ${lastName} and I am ${thisYear - birthYear} years old.`
 
