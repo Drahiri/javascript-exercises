64b446888f78b73754cd6c5a089098705b760e50
Drahiri <kodzidig@o2.pl>
2026-05-05 13:42:52 +0200

    Remove multiple instances of number

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,7 +1,9 @@
 const removeFromArray = function (arr, ...nums) {
     for (const num of nums) {
-        const index = arr.indexOf(num);
-        arr.splice(index, 1);
+        while (arr.includes(num)) {
+            const index = arr.indexOf(num);
+            arr.splice(index, 1);
+        }
     }
 
     return arr;
