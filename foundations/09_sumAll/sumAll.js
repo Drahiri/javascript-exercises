39816132e063bcadf56167e7503f0bc8281deb40
Drahiri <kodzidig@o2.pl>
2026-05-05 13:59:37 +0200

    sumAll works with larger number first

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,4 +1,10 @@
 const sumAll = function (min, max) {
+    if (min > max) {
+        const temp = min;
+        min = max;
+        max = temp;
+    }
+
     let sum = 0;
     for (let i = min; i <= max; i++) {
         sum += i;
