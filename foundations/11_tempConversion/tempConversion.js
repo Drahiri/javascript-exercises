8e4fbea2a083b908abb6870b34068d1dc0e02170
Drahiri <kodzidig@o2.pl>
2026-05-06 08:27:14 +0200

    Convert to Fahrenheit

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -3,7 +3,9 @@ const convertToCelsius = function (fahrenheit) {
   return Math.round(inCelsius * 10) / 10;
 };
 
-const convertToFahrenheit = function () {
+const convertToFahrenheit = function (celsius) {
+  const inFahrenheit = celsius * 9 / 5 + 32;
+  return Math.round(inFahrenheit * 10) / 10;
 };
 
 // Do not edit below this line
