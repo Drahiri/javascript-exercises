18ffc1e88cc22537d97a7961f1f523df2f550b26
Drahiri <kodzidig@o2.pl>
2026-05-12 13:42:10 +0200

    Calculator can calculate power of number

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -14,8 +14,8 @@ const multiply = function (arr) {
   return arr.reduce((acc, val) => acc * val, 1);
 };
 
-const power = function () {
-
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
 const factorial = function () {
