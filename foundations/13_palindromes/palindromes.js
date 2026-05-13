532a13113051bb7e8b38c51e254da0aa449260e9
Drahiri <kodzidig@o2.pl>
2026-05-13 08:21:50 +0200

    Detect palindromes with punctuation

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,20 @@
+const ALPHANUMERIC = "abcdefghijklmnopqrstuvwxyz1234567890";
+
 const palindromes = function (text) {
-    return text === text.split("").reverse().join("");
+    const smallLetters = text.toLowerCase();
+    let noPunctuation = "";
+    for (let letter of smallLetters) {
+        if (ALPHANUMERIC.includes(letter)) {
+            noPunctuation += letter;
+        }
+    }
+
+    const reversed = noPunctuation.split("").reverse().join("");
+
+    return noPunctuation === reversed;
 };
+
+palindromes('A car, a man, a maraca.');
 
 // Do not edit below this line
 module.exports = palindromes;
