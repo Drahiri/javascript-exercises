fc8e36f203e8dc0a089937980333cda129827b4f
Drahiri <kodzidig@o2.pl>
2026-05-13 09:07:52 +0200

    Finds the oldest person

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,9 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (people) {
+    return people.reduce((oldest, current) => {
+        const oldestLived = oldest.yearOfDeath - oldest.yearOfBirth;
+        const currentLived = current.yearOfDeath - current.yearOfBirth;
+        return oldestLived > currentLived ? oldest : current;
+    }, people[0]);
 };
 
 // Do not edit below this line
