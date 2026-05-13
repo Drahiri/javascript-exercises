552cbf88fb4229319fd8bf2f6f3432ed9bccdaff
Drahiri <kodzidig@o2.pl>
2026-05-13 09:28:57 +0200

    Finds the oldest person without year of death

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,7 +1,16 @@
+function getAge(birth, death) {
+    if (!death) {
+        death = new Date().getFullYear();
+    }
+
+    return death - birth;
+}
+
 const findTheOldest = function (people) {
     return people.reduce((oldest, current) => {
-        const oldestLived = oldest.yearOfDeath - oldest.yearOfBirth;
-        const currentLived = current.yearOfDeath - current.yearOfBirth;
+        const oldestLived = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentLived = getAge(current.yearOfBirth, current.yearOfDeath);
+
         return oldestLived > currentLived ? oldest : current;
     }, people[0]);
 };
