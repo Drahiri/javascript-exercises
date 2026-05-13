7ce22014ea9751c869475caf3b96bea15460f628
Drahiri <kodzidig@o2.pl>
2026-05-13 08:41:21 +0200

    Calculate fibonacci sequence

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+let cache = { 0: 0, 1: 1 };
 
+const fibonacci = function (n) {
+    if (n in cache) {
+        return cache[n];
+    }
+    else {
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2);
+        return cache[n];
+    }
 };
 
 // Do not edit below this line
